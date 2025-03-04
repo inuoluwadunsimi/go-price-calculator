fb75c6c986e2fafd2930cf1641ffaa2a368107cd
Inuoluwadunsimi <danielolaoladeinde@gmail.com>
2025-03-04 12:51:40 +0100

    feat(file-manager): outsource file reading to a separate package

--- prices/prices.go
+++ prices/prices.go
@@ -1,10 +1,9 @@
 package prices
 
 import (
-	"bufio"
 	"fmt"
 	"github.com/inuoluwadunsimi/price-calculator/conversion"
-	"os"
+	"github.com/inuoluwadunsimi/price-calculator/fillemanager"
 )
 
 type TaxIncludedPriceJob struct {
@@ -27,29 +26,11 @@ func (job *TaxIncludedPriceJob) Process() {
 }
 
 func (job *TaxIncludedPriceJob) loadData() {
-	file, err := os.Open("prices.txt")
 
+	lines, err := fillemanager.Readlines("prices.txt")
 	if err != nil {
-		fmt.Println("An error occured")
 		fmt.Println(err)
-		return
 	}
-	scanner := bufio.NewScanner(file)
-
-	var lines []string
-
-	for scanner.Scan() {
-		lines = append(lines, scanner.Text())
-	}
-
-	err = scanner.Err()
-	if err != nil {
-		fmt.Println("reading file content failed")
-		fmt.Println(err)
-		file.Close()
-		return
-	}
-
 	prices, err := conversion.StringsToFloat(lines)
 
 	if err != nil {
@@ -59,7 +40,6 @@ func (job *TaxIncludedPriceJob) loadData() {
 	}
 
 	job.InputPrices = prices
-	file.Close()
 }
 
 func NewTaxIncludedPriceJob(taxRate float64) *TaxIncludedPriceJob {
